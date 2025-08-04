57f776a64b36ee27c5058c41a3775b0edefe824f
Hieu Nguyen <144506842+nqhieu2005@users.noreply.github.com>
2025-08-04 10:52:27 +0700

    add api filters

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use(cors());
 app.use(express.json());
 
 const API_DOMAIN = 'https://phimapi.com';
-const PLAYER_DOMAIN = 'https://player.phimapi.com';
+// const PLAYER_DOMAIN = 'https://player.phimapi.com';
 
 app.get("/", async (req, res) => {
     res.status(200).json({message: "Welcome to the Movie API"});
@@ -26,7 +26,7 @@ app.get("/api/movies/new", async(req, res) => {
     }
 });
 
-app.get("/api/movies/:slug", async(req, res) =>{
+app.get("/api/movies/slug/:slug", async(req, res) =>{
     const slug = req.params.slug;
     try {
         const response = await axios.get(`${API_DOMAIN}/phim/${slug}`);
@@ -57,6 +57,24 @@ app.get("/api/country", async(req, res) =>{
         res.status(500).json({ message: "Error fetching movies country" });
     }
 });
+
+app.get("/api/movies/:type_list", async (req, res) => {
+    try {
+        const type_list = req.params.type_list;
+        const queryParams = req.query;
+
+        if (!type_list) {
+            return res.status(400).json({ message: "type_list is required" });
+        }
+        const response = await axios.get(`${API_DOMAIN}/v1/api/danh-sach/${type_list}`, {
+            params: queryParams
+        });
+        // console.log("Query Params: ", queryParams);
+        res.json(response.data);
+    } catch (error) {
+        res.status(500).json({ message: "Error fetching filtered movies", error: error.message }); 
+    }
+})
 
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
